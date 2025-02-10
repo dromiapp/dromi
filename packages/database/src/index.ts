a1f81bc0691c6517d33b6fa631366037b6fc7a21
nmartin-ca <github@nmartin.ca>
2025-02-09 21:17:27 -0800

    chore(packages/database): format

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,12 +1,12 @@
-export { prisma } from './client' // exports instance of prisma
-export * from "../generated/client" // exports generated types from prisma
-export * as Typebox from "../generated/typebox/barrel" // exports generated types from prisma
+export { prisma } from "./client"; // exports instance of prisma
+export * from "../generated/client"; // exports generated types from prisma
+export * as Typebox from "../generated/typebox/barrel"; // exports generated types from prisma
 
 export enum permissionFlag {
-  NONE = 0,
-  VIEW = 1,
-  EDIT = 2,
-  CREATE = 4,
-  DELETE = 8,
-  MANAGE = 16,
+	NONE = 0,
+	VIEW = 1,
+	EDIT = 2,
+	CREATE = 4,
+	DELETE = 8,
+	MANAGE = 16,
 }
