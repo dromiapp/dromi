335234a2e11cdecd53b85516f00cf27f92cbe17d
luke <59530191+luuukee@users.noreply.github.com>
2025-02-12 06:18:59 -0500

    refactor(web): useUser -> use-user

--- apps/web/src/hooks/use-user.ts
+++ apps/web/src/hooks/use-user.ts
@@ -1,7 +1,9 @@
 import api from "~/lib/api";
 
 export function useUser() {
-  const { data, isError, isLoading } = api.useQuery("get", "/auth/me");
+  const { data, isError, isLoading } = api.useQuery("get", "/auth/me", {}, {
+    staleTime: 60 * 60 * 1000,
+  });
 
   return {
     user: data?.user,
