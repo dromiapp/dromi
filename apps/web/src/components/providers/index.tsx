4eb189da75c00ed291c0edd346eff1ff51af75bc
luke <59530191+luuukee@users.noreply.github.com>
2025-02-16 11:02:35 -0500

    chore(web): dont refetch queries on window focus

--- apps/web/src/components/providers/index.tsx
+++ apps/web/src/components/providers/index.tsx
@@ -4,7 +4,13 @@ import { ThemeProvider } from "@repo/ui/components/providers/theme";
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { Toaster } from "@repo/ui/components/ui/sonner";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    }
+  }
+})
 
 export default function Providers({ children }: React.PropsWithChildren<{}>) {
   return (
